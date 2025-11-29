dac4b6746326471359eee505cbf0eacc7b8367bd
AlanFrancis <Alanfrancis11AH@gmail.com>
2025-11-29 16:31:42 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = "https://exoplanet-backend-4-v31o.onrender.com/api";
 
 // Method Switching
 const methodButtons = document.querySelectorAll('.method-btn');
